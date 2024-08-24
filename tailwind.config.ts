b94dc6edc5b52f3a067e9c8f1c6ca17a4c0440e5
nicogulo <nicogulobelas@gmail.com>
2024-08-24 18:54:08 +0700

    feat: added new color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
         extend: {
             fontFamily: {
-                primary: ['Inter', ...defaultTheme.fontFamily.sans]
+                IBM_Plex_Sans: 'var(--font-IBM_Plex_Sans)'
             },
             colors: {
                 primary: {
@@ -22,6 +21,60 @@ export default {
                     800: 'rgb(var(--tw-color-primary-800) / <alpha-value>)',
                     900: 'rgb(var(--tw-color-primary-900) / <alpha-value>)',
                     950: 'rgb(var(--tw-color-primary-950) / <alpha-value>)'
+                },
+                secondary: {
+                    // Customize it on globals.css :root
+                    50: 'rgb(var(--tw-color-secondary-50) / <alpha-value>)',
+                    100: 'rgb(var(--tw-color-secondary-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-secondary-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-secondary-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-secondary-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-secondary-500) / <alpha-value>)'
+                },
+                gray: {
+                    // Customize it on globals.css :root
+                    50: 'rgb(var(--tw-color-gray-50) / <alpha-value>)',
+                    100: 'rgb(var(--tw-color-gray-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-gray-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-gray-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-gray-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-gray-500) / <alpha-value>)',
+                    600: 'rgb(var(--tw-color-gray-600) / <alpha-value>)',
+                    700: 'rgb(var(--tw-color-gray-700) / <alpha-value>)',
+                    800: 'rgb(var(--tw-color-gray-800) / <alpha-value>)',
+                    900: 'rgb(var(--tw-color-gray-900) / <alpha-value>)'
+                },
+                success: {
+                    // Customize it on globals.css :root
+                    100: 'rgb(var(--tw-color-success-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-success-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-success-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-success-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-success-500) / <alpha-value>)'
+                },
+                error: {
+                    // Customize it on globals.css :root
+                    100: 'rgb(var(--tw-color-error-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-error-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-error-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-error-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-error-500) / <alpha-value>)'
+                },
+                warning: {
+                    // Customize it on globals.css :root
+                    100: 'rgb(var(--tw-color-warning-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-warning-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-warning-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-warning-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-warning-500) / <alpha-value>)'
+                },
+                info: {
+                    // Customize it on globals.css :root
+                    100: 'rgb(var(--tw-color-info-100) / <alpha-value>)',
+                    200: 'rgb(var(--tw-color-info-200) / <alpha-value>)',
+                    300: 'rgb(var(--tw-color-info-300) / <alpha-value>)',
+                    400: 'rgb(var(--tw-color-info-400) / <alpha-value>)',
+                    500: 'rgb(var(--tw-color-info-500) / <alpha-value>)'
                 },
                 dark: '#222222'
             },

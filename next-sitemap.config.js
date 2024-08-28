df4397d9284c4e25251773082938a79bdc4f99c9
nicogulo <nicogulobelas@gmail.com>
2024-08-28 12:21:36 +0700

    fix: handle seo

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,8 +4,7 @@
  */
 module.exports = {
     // !STARTERCONF Change the siteUrl
-    /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-    siteUrl: 'https://tsnext-tw.thcl.dev',
+    siteUrl: 'https://www.binalokaindonesia.com',
     generateRobotsTxt: true,
     robotsTxtOptions: {
         policies: [{ userAgent: '*', allow: '/' }]
--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -5,17 +5,16 @@ import { useRouter } from 'next/router';
 const defaultMeta = {
     title: 'Binaloka Indonesia',
     siteName: 'Binaloka Indonesia',
-    description:
-        'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    description: '',
     /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-    url: 'https://tsnext-tw.thcl.dev',
+    url: 'https://www.binalokaindonesia.com/',
     type: 'website',
     robots: 'follow, index',
     /**
      * No need to be filled, will be populated with openGraph function
      * If you wish to use a normal image, just specify the path below
      */
-    image: 'https://tsnext-tw.thcl.dev/images/large-og.png'
+    image: 'https://www.binalokaindonesia.com/images/large-og.png'
 };
 
 type SeoProps = {

20281669259cd4c975b9cb83798c166c0a2d9f39
nicogulo <nicogulobelas@gmail.com>
2024-12-04 23:18:58 +0700

    fix: styling height skeleton

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -7,7 +7,7 @@ type SkeletonProps = React.ComponentPropsWithoutRef<'div'>;
 export default function Skeleton({ className, ...rest }: SkeletonProps) {
     return (
         <div
-            className={cn('animate-shimmer bg-[#f6f7f8]', className)}
+            className={cn('animate-shimmer h-fit min-h-5 bg-[#f6f7f8]', className)}
             style={{
                 backgroundImage: 'linear-gradient(to right, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%)',
                 backgroundSize: '700px 100%',

0c3dbf92d33fb6589bf2cc0092c7ec1e61c5a2f5
nicogulo <nicogulobelas@gmail.com>
2024-12-04 23:21:00 +0700

    fix: env config

--- src/constant/env.ts
+++ src/constant/env.ts
@@ -1,7 +1,7 @@
 export const isProd = process.env.NODE_ENV === 'production';
 export const isLocal = process.env.NODE_ENV === 'development';
 
-export const showLogger = isLocal ? true : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true' ?? false;
+export const showLogger = isLocal ? true : process.env.NEXT_PUBLIC_SHOW_LOGGER === 'true';
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 export const APP_ENV = process.env.NEXT_PUBLIC_APP_ENV;

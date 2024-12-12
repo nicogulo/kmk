e52d21a150cf99c676b5e83f6d0d9f177e173f4f
nicogulo <nicogulobelas@gmail.com>
2024-12-12 17:02:17 +0700

    test: add toast error

--- src/modules/Profile/Kyc/components/UploadDocument.tsx
+++ src/modules/Profile/Kyc/components/UploadDocument.tsx
@@ -34,13 +34,7 @@ const RenderUploadDocumentKtp = ({ handleUpload }: RenderUploadDocumentProps) =>
                     Klik untuk mengunggah atau seret dan lepas <br />
                     PNG, JPG or PDF (max. 5 MB)
                 </p>
-                <input
-                    type='file'
-                    className='hidden'
-                    accept='image/*'
-                    capture='user'
-                    onChange={(e: any) => handleUpload(e)}
-                />
+                <input type='file' className='hidden' accept='image/*' onChange={(e: any) => handleUpload(e)} />
             </div>
         </label>
     </div>
@@ -69,9 +63,7 @@ const UploadDocument: React.FC<Props> = ({ setTab }) => {
 
     const handleUpload = (e: any) => {
         const file = e.target.files[0];
-        if (file) {
-            toast.error(e);
-        }
+
         toast.error(e);
         if (file && file.size > 5242880) {
             toast.error('Ukuran file tidak boleh melebihi 5MB.');

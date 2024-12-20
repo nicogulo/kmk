02a65499ce8bcdd3938e06bae76a8e4becb849bc
nicogulo <nicogulobelas@gmail.com>
2024-12-20 22:45:01 +0700

    fix: add button back

--- src/modules/TermsCondition/TermsCondition.tsx
+++ src/modules/TermsCondition/TermsCondition.tsx
@@ -1,8 +1,10 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 
 import Container from '@/components/Container';
+import Icons from '@/components/Icon';
 import { Else, If, Then } from '@/components/If';
 
 import Footer from '@/modules/Home/components/Footer';
@@ -10,6 +12,7 @@ import EnglishVersion from '@/modules/TermsCondition/components/EnglishVersion';
 import IndoVersion from '@/modules/TermsCondition/components/IndoVersion';
 
 const TermsCondition = () => {
+    const router = useRouter();
     const { i18n } = useTranslation();
     const isLangId = i18n.language === 'id' ? true : false;
 
@@ -21,6 +24,10 @@ const TermsCondition = () => {
             </Head>
             <Container className='flex justify-center py-3'>
                 <div className='tnc-text flex flex-col xl:w-[50%]'>
+                    <div className='flex flex-row items-center gap-2 pb-2' onClick={() => router.back()}>
+                        <Icons icon='ArrowLeft' width={20} height={20} />
+                        <span className='text-base text-gray-700'>Kembali</span>
+                    </div>
                     <If condition={isLangId}>
                         <Then>
                             <IndoVersion />
